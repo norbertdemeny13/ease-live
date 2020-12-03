9f3db6a0e14538f68ad8bae0ce127fa60bff3559
Norbert Demeny <norbi.demeny.3pg@cofense.com>
2020-12-03 17:58:40 +0200

    Live Ease

---
+++ js/home.a15ab3bb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["home"],{bb51:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"home"},[t("h1",[e._v("This is an home page")])])}],c=t("2877"),i={},l=Object(c["a"])(i,s,a,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=home.a15ab3bb.js.map
